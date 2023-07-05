d0764fdeab3089c0b7ca49027ae45b8f8ddbf28c
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-07-05 18:31:43 -0500

    Created temporary first question and answer choices

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// Declares initial variables and elements to be utilized in later functions
+var mainEl = document.querySelector("main");
+var h3El = document.querySelector("h3");
+var startButton = document.querySelector("#startButton");
+var answer1Button = document.createElement("button");
+var answer2Button = document.createElement("button");
+var answer3Button = document.createElement("button");
+var answer4Button = document.createElement("button");
+
+// Starts the quiz when the user clicks the Start Button
+startButton.addEventListener("click", function() {
+    
+    // Change the text to display Question 1
+    h3El.textContent = "What is the answer to Question 1?"
+
+    // Remove the Start Button and add buttons for 4 answer choices
+    mainEl.removeChild(startButton);
+    mainEl.appendChild(answer1Button);
+    mainEl.appendChild(answer2Button);
+    mainEl.appendChild(answer3Button);
+    mainEl.appendChild(answer4Button);
+    
+    // Place text in the new buttons for each of the answers
+    answer1Button.textContent = "Choose me!";
+    answer2Button.textContent = "Don't choose me!";
+    answer3Button.textContent = "Don't choose me either!";
+    answer4Button.textContent = "Definitely don't choose me!";
+});
